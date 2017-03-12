73c3c98de0394ad3fe979e0d57709bb6f553aaba
emmanuel delgado <larin.s931@gmail.com>
2017-03-12 11:15:39 -0700

    Extract scoring strategies into its own file

--- game_agent.py
+++ game_agent.py
@@ -9,37 +9,12 @@ relative strength using tournament.py and include the results in your report.
 import random
 import pdb
 from collections import deque
-
+from scoring import custom_score
 
 
 class Timeout(Exception):
     """Subclass base exception for code clarity."""
     pass
-
-
-def custom_score(game, player):
-    """Calculate the heuristic value of a game state from the point of view
-    of the given player.
-
-    Note: this function should be called from within a Player instance as
-    `self.score()` -- you should not need to call this function directly.
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : object
-        A player instance in the current game (i.e., an object corresponding to
-        one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-    Returns
-    -------
-    float
-        The heuristic value of the current game state to the specified player.
-    """
-    return float(len(game.get_legal_moves()))
 
 
 class CustomPlayer:
---
+++ scoring.py
@@ -0,0 +1,34 @@
+"""This file contains all the scoring strategies
+"""
+
+def number_of_moves(game, player):
+    """
+    Calculates a score based on the number of available moves for the current
+    player
+    """
+    return float(len(game.get_legal_moves()))
+
+
+def custom_score(game, player):
+    """Calculate the heuristic value of a game state from the point of view
+    of the given player.
+
+    Note: this function should be called from within a Player instance as
+    `self.score()` -- you should not need to call this function directly.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+
+    Returns
+    -------
+    float
+        The heuristic value of the current game state to the specified player.
+    """
+    return number_of_moves(game, player)
